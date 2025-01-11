70b059b4bdd07c5eb30b2cc27ca60f63d4889eb9
Nishank Kesharwani <shivakeshri51@gmail.com>
2025-01-11 23:33:39 +0530

    Add files via upload

---
+++ caclucator.py
@@ -0,0 +1,9 @@
+a=float(input("enter value of a:"))
+b=float(input("enter value of b:"))
+c=input('enter operator(+,*,-,/):')
+if(c=='*'):
+    d=a*b
+    print('multi.=',d)
+if(c=='/'):
+    d=a/b
+    print("devide=",d)
